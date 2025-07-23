294c9adba1c3d46fe649b589920ffeef727cd9f5
Vasanth-0706 <vasanthcisco07@gmail.com>
2025-07-23 10:39:25 +0530

    :changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S11-Skill3/">
+    <Router basename="/S111-Skill3/">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/S11-Skill3/'
+  base: '/S111-Skill3/'
 });
